b1300119a9ddbd0c7c67b6e6309d1e0a80880b57
David Frank <davidfrank96.df@gmail.com>
2019-03-18 11:18:35 -0700

    ajustments to both login and signup pages

--- UI/js/app.js
+++ UI/js/app.js
@@ -1,5 +1,6 @@
 const overlay = document.querySelector('.overlay');
 const mobileMenu = document.querySelector('.mobile-menu');
+//const checkbox = document.getElementById('agree').checked;
 
 const redirect = location => {
     window.location.href = `${location}.html`;
@@ -27,3 +28,17 @@ overlay.addEventListener("click", () => {
 
 mobileMenu.addEventListener("click", toggleMobileNav);
 
+
+
+//const check = () => {
+//    const checkbox = document.getElementById('agree');
+//    if (document.getElementById('agree').checked) {
+//        return true;
+//    } else {
+//        alert('Please indicate that you have read and agree to the Terms and Conditions and Privacy Policy');
+//        return false;
+//    }
+//    
+//}
+
+//checkbox.addEventListener("click", check);
